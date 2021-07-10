740c3c64175f89d643c9e7ab8b0fe6719ee19952
Bruno Falcão <bruno.c.falcao@brunofalcao.me>
2021-07-11 00:58:50 +0200

    wip

--- resources/views/prelaunched/default.blade.php
+++ resources/views/prelaunched/default.blade.php
@@ -4,7 +4,9 @@
         <link rel="preconnect" href="https://fonts.gstatic.com">
         <link href="https://fonts.googleapis.com/css2?family=Nunito+Sans:ital,wght@0,200;0,300;0,400;0,600;0,700;0,800;0,900;1,200;1,300;1,400;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet">
         <link href="https://fonts.googleapis.com/css2?family=Zen+Dots&display=swap" rel="stylesheet">
+
         <!-- Social media -->
+        <!--
         <meta name="twitter:card" content="summary_large_image" />
         <meta name="twitter:site" content="@brunocfalcao" />
         <meta name="twitter:title" content="Nova Advanced UI" />
@@ -17,6 +19,7 @@
         <meta property="og:description" content="Beautiful UI components by the creators of Tailwind CSS." />
         <meta property="og:image" content="https://tailwindui.com/img/og-image.png" />
         <meta property="description" content="Beautiful UI components by the creators of Tailwind CSS." />
+        -->
         <!-- /Social media -->
 
         <!-- Favicon --> 
@@ -31,7 +34,7 @@
             <!-- Logo section -->
             <section class="bg-background-900 px-6 lg:px-8 py-8">
                 <div class="max-w-screen-xl mx-auto flex justify-center items-center">
-                    <img class="h-12" src="/vendor/nova-advanced-ui/images/logo.png" alt="{{ course()::name() }}">
+                    <img class="h-12" src="/vendor/nova-advanced-ui/images/logo.png" alt="{{ course()->name }}">
                 </div>
             </section>
             <!-- /Logo section -->
