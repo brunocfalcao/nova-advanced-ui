2d98c72194afa696fb4533d63f400d208cf5f0f7
Bruno Falcão <bruno.c.falcao@brunofalcao.me>
2021-07-12 21:49:10 +0200

    wip

--- webpack.mix.js
+++ webpack.mix.js
@@ -2,17 +2,11 @@ const mix = require('laravel-mix');
 
 /*
  |--------------------------------------------------------------------------
- | Mix Asset Management
+ | Eduka asset management using Laravel Mix
  |--------------------------------------------------------------------------
  |
- | Mix provides a clean, fluent API for defining some Webpack build steps
- | for your Laravel applications. By default, we are compiling the CSS
- | file for the application as well as bundling up all the JS files.
- |
  */
-
-mix.copy('resources/js/retina.min.js', 'public/vendor/nova-advanced-ui/js')
-   .copy('resources/js/retina.min.js', 'resources/overrides/public/vendor/nova-advanced-ui/js')
-   .postCss('resources/css/app.css', 'public/vendor/nova-advanced-ui/css', [
+mix.copy('resources/js/retina.min.js', 'resources/overrides/public/vendor/nova-advanced-ui/js')
+   .postCss('resources/css/app.css', 'resources/overrides/public/vendor/nova-advanced-ui/css', [
       require("tailwindcss")
    ]);
