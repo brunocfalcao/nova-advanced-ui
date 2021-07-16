6043ec1bad8523489b7b0d7d3b3a7f9320c80487
Bruno Falcão <bruno.c.falcao@brunofalcao.me>
2021-07-16 14:39:26 +0200

    wip

---
+++ resources/overrides/config/eduka-nereus.php
@@ -0,0 +1,21 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Your course information
+    |--------------------------------------------------------------------------
+    |
+    | This file is for storing information regarding your course.
+    | Most of this information can be placed in environment variables.
+    | Remember that you can change these values to dynamic class values in
+    | case you need more logic on them.
+    |
+    */
+
+    'mail' => [
+        'subscribed' => \Eduka\NovaAdvancedUI\Mail\ThankYouForSubscribing::class,
+    ],
+
+];
