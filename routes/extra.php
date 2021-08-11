7608685ac30f8ccec039db352365bb4f805d5cb8
Bruno Falcão <bruno.c.falcao@brunofalcao.me>
2021-08-11 16:10:55 +0200

    Update extra.php

--- routes/extra.php
+++ routes/extra.php
@@ -5,7 +5,6 @@ use Illuminate\Support\Facades\Mail;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/email', function () {
-
     Mail::to('bruno.falcao@live.com')
         ->send(new ThankYouForSubscribing());
 
